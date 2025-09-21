315feaaaacdfdf8f5e6f9b3f04b02d5f1ee0857f
K Puneeth <kunkupuneeth6303@gmail.com>
2025-09-21 23:26:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (31.72%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int i = s.length() - 1;
+        // skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') i--;
+        int len = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+        return len;
+    }
+}
